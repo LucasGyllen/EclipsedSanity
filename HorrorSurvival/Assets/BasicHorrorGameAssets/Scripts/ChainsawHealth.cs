7eebf3accc08cee15cffc47a5fba6a228f7a6b23
Elias H <98331725+Suedie@users.noreply.github.com>
2024-02-19 19:32:18 +0100

    Add .gitignore and more sounds

--- HorrorSurvival/Assets/BasicHorrorGameAssets/Scripts/ChainsawHealth.cs
+++ HorrorSurvival/Assets/BasicHorrorGameAssets/Scripts/ChainsawHealth.cs
@@ -4,6 +4,12 @@ public class ChainsawHealth : MonoBehaviour, IDamageable
 {
     [SerializeField] private float health = 200f;
 
+    public AudioClip deathSound;
+
+    private AudioSource audioSource;
+
+    private bool isDead = false;
+
     public void Damage(float damage)
     {
         Debug.Log($"Enemy took {damage} damage.");
@@ -12,6 +18,11 @@ public class ChainsawHealth : MonoBehaviour, IDamageable
         if (health <= 0)
         {
             Die();
+            if (!isDead)
+            {
+                PlaySound(deathSound);
+                isDead = true;
+            }
         }
     }
 
@@ -23,5 +34,15 @@ public class ChainsawHealth : MonoBehaviour, IDamageable
 
         // For example, you might destroy the enemy GameObject:
         gameObject.GetComponent<Animator>().SetBool("Death", true);
+
+    }
+
+    private void PlaySound(AudioClip soundClip)
+    {
+        if (!audioSource.isPlaying || audioSource.clip != soundClip)
+            {
+                audioSource.clip = soundClip;
+                audioSource.Play();
+            }
     }
 }
